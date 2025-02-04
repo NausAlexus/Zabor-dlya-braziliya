e71ea6f52f6e9935dea3c1c11cb015c48f1de3be
DenisBardashevich <ackapbinka@mail.ru>
2025-02-04 22:33:23 +0300

    fix slider

--- script/sliderPortfolio.js
+++ script/sliderPortfolio.js
@@ -1,9 +1,9 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
+const slidesP = document.querySelectorAll('.slide');
 
 function showSlide(index) {
 
-    const slideWidth = slides[0].clientWidth;
+    const slideWidth = slidesP[0].clientWidth;
     const offset = index * slideWidth;
     document.querySelector('.slider').style.transform = `translateX(-${offset}px)`;
 }
@@ -11,8 +11,8 @@ function showSlide(index) {
 function changeSlide(direction) {
     currentSlide += direction;
 
-    if (currentSlide < 0) currentSlide = slides.length - 2;
-    if (currentSlide >= slides.length - 1) currentSlide = 0;
+    if (currentSlide < 0) currentSlide = slidesP.length - 2;
+    if (currentSlide >= slidesP.length - 1) currentSlide = 0;
     showSlide(currentSlide);
 }
 
